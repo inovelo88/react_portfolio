98242da1356a86e0d6d669b59e32b44b461cd8a1
Irene Novelo <iyn88@yahoo.com>
2021-01-05 19:05:16 -0800

    added fontawesome link

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,10 +5,9 @@ const Footer = () => {
     return (
         <footer>
 			<div className="social-icons">
-				<a href="https://www.linkedin.com/in/irene-novelo-6205396a/" target="_blank" rel="noreferrer"><i
-						className="fab fa-linkedin"></i></a>
-				<a href="https://www.twitter.com/pandy8824" target="_blank" rel="noreferrer"><i className="fab fa-twitter"></i></a>
-				<a href="https://instagram.com/introducing_pickle" target="_blank" rel="noreferrer"><i className="fab fa-instagram"></i></a>
+				<a href="https://www.linkedin.com/in/irene-novelo-6205396a/" target="_blank" rel="noreferrer"><i className="fa fa-linkedin"></i></a>
+				<a href="https://www.twitter.com/pandy8824" target="_blank" rel="noreferrer"><i className="fa fa-twitter"></i></a>
+				<a href="https://instagram.com/introducing_pickle" target="_blank" rel="noreferrer"><i className="fa fa-instagram"></i></a>
 			</div>
 		</footer>
     )
--- src/Components/Social.js
+++ src/Components/Social.js
@@ -1,15 +1,15 @@
 import React from "react";
+import 'font-awesome/css/font-awesome.min.css'
 
 
 const Social = () => {
-    return (
-        <div className="socialscroll">
-			<a href="https://www.linkedin.com/in/irene-novelo-6205396a/" target="_blank" rel="noreferrer"><i
-					class="fab fa-linkedin"></i></a>
-			<a href="https://www.twitter.com/pandy8824" target="_blank" rel="noreferrer"><i className="fab fa-twitter"></i></a>
-			<a href="https://instagram.com/introducing_pickle" target="_blank" rel="noreferrer"><i className="fab fa-instagram"></i></a>
+	return (
+		<div className="socialscroll">
+			<a href="https://www.linkedin.com/in/irene-novelo-6205396a/" target="_blank" rel="noreferrer"><i className="fa fa-linkedin"></i></a>
+			<a href="https://www.twitter.com/pandy8824" target="_blank" rel="noreferrer"><i className="fa fa-twitter"></i></a>
+			<a href="https://instagram.com/introducing_pickle" target="_blank" rel="noreferrer"><i className="fa fa-instagram"></i></a>
 		</div>
-    )
+	)
 }
 
 export default Social
